1702e99561c14ea64ee58ceda0b16774fa3c79e2
Kjgwo <woo052435@gmail.com>
2022-04-11 20:30:41 +0900

    글에 헤더 이미지 넣기 & 파일 넣기

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page),
-    path('', views.index),
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    #path('<int:pk>/', views.single_post_page),
+    #path('', views.index),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
