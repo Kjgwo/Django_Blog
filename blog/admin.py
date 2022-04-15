e3d45127f0f3812438a37b686eafd2f8c707c955
Kjgwo <woo052435@gmail.com>
2022-04-16 01:02:16 +0900

    글 쓰기/업데이트 기능 추가, 글쓰기 뷰/업데이트 뷰/로그인 체크 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,47 +1,21 @@
-<<<<<<< HEAD
 from django.contrib import admin
 from .models import Post, Category, Tag
 
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
-    # prepopulated_fields: name 필드 값으로 slug를 자동생성하도록 설정
-
 
 class TagAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
-# Register your models here.
-
-admin.site.register(Post)
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Tag, TagAdmin)
-
-
-
-
-=======
-from django.contrib import admin
-from .models import Post, Category, Tag
-
-
-class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name',)}
-    # prepopulated_fields: name 필드 값으로 slug를 자동생성하도록 설정
-
-
-class TagAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name',)}
 
 
 # Register your models here.
-
 admin.site.register(Post)
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Tag, TagAdmin)
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Tag,TagAdmin)
 
 
 
 
->>>>>>> 2b873285fa3dcb81adb3d0df8df345db7fc979d7
--- single_pages/models.py
+++ single_pages/models.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 from django.db import models
 
 # Create your models here.
-=======
-from django.db import models
 
-# Create your models here.
->>>>>>> 2b873285fa3dcb81adb3d0df8df345db7fc979d7
