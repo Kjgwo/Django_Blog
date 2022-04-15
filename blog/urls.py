930a829e6deca0112995597811bb4082e667af9b
김정우 <93897045+Kjgwo@users.noreply.github.com>
2022-04-16 01:08:22 +0900

    글 쓰기/업데이트 기능 추가, 글쓰기 뷰/업데이트 뷰/로그인 체크 추가

---
+++ blog/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.PostList.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    #path('<int:pk>/', views.single_post_page),
+    #path('', views.index),
+    path('category/<str:slug>/', views.categories_page),
+    path('tag/<str:slug>/', views.tag_page),
+    path('create_post/', views.PostCreate.as_view()),
+    path('update_post/<int:pk>/', views.PostUpdate.as_view())
+
+]
---
+++ blog/views.py
@@ -0,0 +1,118 @@
+
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post, Category, Tag
+
+
+# Create your views here.
+
+
+# def index(request):
+#
+#     posts = Post.objects.all().order_by('-pk')
+#
+#
+#     return render(
+#         request,
+#         'blog/post_list.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
+
+
+# def single_post_page(request, pk):
+#
+#     post = Post.objects.get(pk=pk)
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'post': post,
+#         }
+#     )
+
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'hook_msg', 'content', 'head_image', 'attached_file', 'category']
+
+    template_name = "blog/post_form_update.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        current_user = request.user
+        if current_user.is_authenticated and current_user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+
+class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Post
+    fields = ['title', 'hook_msg', 'content', 'head_image', 'attached_file', 'category']
+
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated and (current_user.is_superuser or current_user.is_staff):
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
+
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['count_posts_without_category'] = Post.objects.filter(category=None).count()
+
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['count_posts_without_category'] = Post.objects.filter(category=None).count()
+
+        return context
+
+
+def categories_page(request, slug):
+    if slug == 'no-category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    context = {
+        'categories': Category.objects.all(),
+        'count_posts_without_category': Post.objects.filter(category=None).count(),
+        'category': category,
+        'post_list': post_list
+    }
+
+    return render(request, 'blog/post_list.html', context)
+
+
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    context = {
+        'categories': Category.objects.all(),
+        'count_posts_without_category': Post.objects.filter(category=None).count(),
+        'tag': tag,
+        'post_list': post_list
+    }
+    return render(request, 'blog/post_list.html', context)
