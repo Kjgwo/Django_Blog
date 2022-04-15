cb641f22002629dbc35fdb3c07b030e2149a808a
Kjgwo <woo052435@gmail.com>
2022-04-16 01:09:50 +0900

    글 쓰기/업데이트 기능 추가, 글쓰기 뷰/업데이트 뷰/로그인 체크 추가

--- single_pages/tests.py
+++ single_pages/tests.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 from django.test import TestCase
 
 # Create your tests here.
-=======
-from django.test import TestCase
 
-# Create your tests here.
->>>>>>> 2b873285fa3dcb81adb3d0df8df345db7fc979d7
